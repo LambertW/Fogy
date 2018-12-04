65eb17a59e084055baa25f71646cb4bde97d0684
Lambert Wu <plclambertwu@outlook.com>
2018-12-05 00:04:27 +0800

    Add objectmapper

---
+++ src/Fogy.AutoMapper/AutoMapperObjectMapper.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using IObjectMapper = Fogy.Core.ObjectMapper.IObjectMapper;
+
+namespace Fogy.AutoMapper
+{
+    public class AutoMapperObjectMapper : IObjectMapper
+    {
+        private readonly IMapper _mapper;
+
+        public AutoMapperObjectMapper(IMapper mapper)
+        {
+            _mapper = mapper;
+        }
+
+        public TDestincation Map<TDestincation>(object source)
+        {
+            return _mapper.Map<TDestincation>(source);
+        }
+
+        public TDestincation Map<TSource, TDestincation>(TSource source, TDestincation destincation)
+        {
+            return _mapper.Map<TSource, TDestincation>(source);
+        }
+    }
+}
